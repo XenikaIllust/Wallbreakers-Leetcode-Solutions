3babaeff46df6e548ae9caaf13d1b7fb0647684d
Zachary Zhao Xing Lim <ZacharyZXLim@users.noreply.github.com>
2019-06-22 00:18:16 -0700

    Update 905-sort_array_by_parity.py

--- Week_1/905-sort_array_by_parity.py
+++ Week_1/905-sort_array_by_parity.py
@@ -1,6 +1,12 @@
 class Solution:
     def sortArrayByParity(self, A: List[int]) -> List[int]:
-        # This solution runs in O(n) time complexity. I think (for now) the only solution is O(n) because the program has to run through each element in the array. The numerical order does not matter. Therefore I created two bins called "even" and "odd" to keep elements of each category, then concatenate both. Space efficiency is O(n) because space only grows as much as # of input elements.
+        '''
+        This solution runs in O(n) time complexity. I think (for now) the only solution is O(n) 
+        because the program has to run through each element in the array. The numerical order 
+        does not matter. Therefore I created two bins called "even" and "odd" to keep elements 
+        of each category, then concatenate both. Space efficiency is O(n) because space only 
+        grows as much as # of input elements.
+        '''
         
         even = []
         odd = []
